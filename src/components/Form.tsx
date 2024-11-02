6b6c65fdad0e8b05a54572c74ed4e0495f3b1d2a
edpau <edpau@me.com>
2024-11-02 02:27:08 +0000

    fix: remove SVG version attribute

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -70,7 +70,6 @@ function Form() {
         <svg
           className="hidden"
           id="definition"
-          version="1.0"
           xmlns="http://www.w3.org/2000/svg"
           aria-hidden="true"
         >
